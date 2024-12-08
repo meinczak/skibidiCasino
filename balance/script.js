8040077a7277dfd08c1c5b5c09ee268bc2be88f0
wojciorini <wojtekaw2006@gmail.com>
2024-12-08 21:16:01 +0100

    poprawki balance

--- balance/script.js
+++ balance/script.js
@@ -1,31 +1,29 @@
-function kwota10(){
-
-var button = document.getElementById("dyszka");
 var output = document.getElementById("numer");
-    
+
+function kwota10(){
 document.getElementById("numer").value = "10";
 }
 
 function kwota50(){
-
-    var button = document.getElementById("psiat");
-    var output = document.getElementById("numer");
-        
     document.getElementById("numer").value = "50";
 }
 
 function kwota100(){
-
-var button = document.getElementById("stowka");
-var output = document.getElementById("numer");
-    
 document.getElementById("numer").value = "100";
 }
 
 function kwota500(){
-
-    var button = document.getElementById("piset");
-    var output = document.getElementById("numer");
-        
     document.getElementById("numer").value = "500";
+}
+
+function pokaz(){
+
+    var amt = document.getElementById("numer").value;
+    if(amt < 1){
+        document.getElementById("error").style.display = "block";
+    }
+    else{
+    document.getElementById("kwota").innerHTML = "The deposit amount is " + amt + "PLN";
+    document.getElementById("error").style.display = "none";
+    }
 }
