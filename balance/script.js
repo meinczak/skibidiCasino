0bfc4810fa7cebc670dddbe516b683eb3196fa0c
wojciorini <wojtekaw2006@gmail.com>
2024-12-08 03:25:33 +0100

    podstrony poprawki

---
+++ balance/script.js
@@ -0,0 +1,31 @@
+function kwota10(){
+
+var button = document.getElementById("dyszka");
+var output = document.getElementById("numer");
+    
+document.getElementById("numer").value = "10";
+}
+
+function kwota50(){
+
+    var button = document.getElementById("psiat");
+    var output = document.getElementById("numer");
+        
+    document.getElementById("numer").value = "50";
+}
+
+function kwota100(){
+
+var button = document.getElementById("stowka");
+var output = document.getElementById("numer");
+    
+document.getElementById("numer").value = "100";
+}
+
+function kwota500(){
+
+    var button = document.getElementById("piset");
+    var output = document.getElementById("numer");
+        
+    document.getElementById("numer").value = "500";
+}
