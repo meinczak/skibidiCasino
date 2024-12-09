53bfe9acbd3ce8903006732d2e85e0f5887121e7
wojciorini <wojtekaw2006@gmail.com>
2024-12-09 14:39:57 +0100

    loading

---
+++ balance/loadingStyle.js
@@ -0,0 +1,27 @@
+var napis = "";
+var kropka = 0;
+function kropki(){
+
+
+    if(kropka<4){
+        if(kropka==3){
+            napis="...";
+            kropka=-1;
+        }
+        else if(kropka==2){
+            napis=".."
+        }
+        else if(kropka==1){
+            napis="."
+        }
+        else if(kropka==0){
+            napis=""
+        }
+        document.getElementById("Loading").innerHTML = "Loading" + napis;
+        kropka++;
+
+    }
+
+}
+
+setInterval(kropki, 1000);
